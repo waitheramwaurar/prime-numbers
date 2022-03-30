6ed7844fd86a0e15a86b6b55a40c12f95d389f49
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-31 00:31:43 +0300

    Corrected code

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 #This is a program that determines whether a number is a prime number
+import math
 
 num = int(input("Enter number: "))
 
 def isPrime(num):
-    if num == 2 or num == 1 or num % 2 == 0:
-        return False
-    else:
+    if num == 2:
         return True
+    elif num == 1 or num % 2 == 0:
+        return False
+    for i in range(3, int(num**0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+
 if isPrime(num) is True:
     print("{} is a prime number".format(num))
 else:
